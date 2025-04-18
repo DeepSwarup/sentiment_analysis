c7b8c08de18c39129d8aa39172b5b6bc52afac25
DeepSwarup <deepswarup1@gmail.com>
2025-04-19 01:32:25 +0530

    remove the mongo issue

--- app/api/analyze/route.js
+++ app/api/analyze/route.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { connectToDatabase } from '../../lib/mongodb';
+// import { connectToDatabase } from '../../lib/mongodb';
 
 export async function POST(request) {
   try {
